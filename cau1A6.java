e177b7c42393092f9d515d41114e33e93817dbe1
jeffau13 <jeffau13@gmail.com>
2019-04-20 20:00:09 -0400

    initial commit

---
+++ cau1A6.java
@@ -0,0 +1,27 @@
+
+/**
+ * author: Jeffrey Au (Undergrad Student) CSC 4222
+ */
+
+import java.util.Random;
+
+public class cau1A6 {
+    public static void main(String[] args) {
+        keyGeneration();
+
+    }
+
+    public static void keyGeneration() {
+
+    }
+
+    public byte[] encryption(int e, int n, String plaintext) {
+        String ciphertext;
+        return ciphertext;
+    }
+
+    public byte[] decryption(int d, int n, String ciphertext) {
+        String plaintext;
+        return plaintext;
+    }
+}
