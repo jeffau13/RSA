daf7a172112851c231f991ba1cdb571f79e7c962
jeffau13 <jeffau13@gmail.com>
2019-04-20 22:47:36 -0400

    added methods for encryption, decryption, and converting byteArrayToString

--- cau1A6.java
+++ cau1A6.java
@@ -4,24 +4,39 @@
  */
 
 import java.util.Random;
+import java.math.BigInteger;
 
 public class cau1A6 {
+
     public static void main(String[] args) {
-        keyGeneration();
 
     }
 
-    public static void keyGeneration() {
-
+    // @params: public key pair (e,n) and plaintext in String form
+    // returns an encrypted byte array
+    public static byte[] encryption(BigInteger e, BigInteger n, String plaintext) {
+        byte[] encrypted = (new BigInteger(plaintext.getBytes())).modPow(e, n).toByteArray();
+        return encrypted;
     }
 
-    public byte[] encryption(int e, int n, String plaintext) {
-        String ciphertext;
-        return ciphertext;
+    // @params: private key pair (d,n) and cyphertext in byte array
+    // returns decrypted String
+    public static String decryption(BigInteger d, BigInteger n, byte[] ciphertext) {
+        byte[] plaintext = (new BigInteger(ciphertext)).modPow(d, n).toByteArray();
+        return new String(plaintext);
     }
 
-    public byte[] decryption(int d, int n, String ciphertext) {
-        String plaintext;
-        return plaintext;
+    // @params: byte[]
+    // returns String of the byte[]
+    public static String byteArrayToString(byte[] bytes) {
+        String str = "";
+        for (byte b : bytes) {
+            str += Byte.toString(b);
+        }
+        return str;
+    }
+
+    public class keyGeneration {
+
     }
 }
